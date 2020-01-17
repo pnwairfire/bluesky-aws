79699390d9ed901d0dafc9cf4b61a96e32c7d59b
Joel Dubowy <jdubowy@gmail.com>
2020-01-16 16:08:15 -0800

    WIP trying to get asyncronous s3 status object fetching code working

--- bluesky-aws-admin/lib/status.js
+++ bluesky-aws-admin/lib/status.js
@@ -2,14 +2,15 @@ const util = require('util');
 
 
 // S3 Docs: https://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/S3.html
-//const AWS = require('aws-sdk');
+const AWS = require('aws-sdk');
 //var S3 = require('aws-sdk/clients/s3');
-import * as AWS from 'aws-sdk';
+//import * as AWS from 'aws-sdk';
 
 const s3 = new AWS.S3();
 const getObject = util.promisify(s3.getObject);
 
-export async function getRequestStatus(bucketName, requestId) {
+//export async function getRequestStatus(bucketName, requestId) {
+exports.getRequestStatus = function (bucketName, requestId) {
     var params = {
         Bucket: bucketName,
         Key: 'status/' + requestId + '-status.json',
@@ -17,6 +18,11 @@ export async function getRequestStatus(bucketName, requestId) {
     };
     console.log('Fetching ' + params.Bucket +'/' + params.Key);
 
-    // TODO: what happens with errors???
-    return await getObject(params)
+    try {
+        //return await getObject(params)''
+        return getObject(params)
+    }
+    catch (err) {
+        console.log('ERROR:', err);
+    }
 }
--- bluesky-aws-admin/pages/api/[request]/status.js
+++ bluesky-aws-admin/pages/api/[request]/status.js
@@ -1,16 +1,29 @@
 import { useRouter } from 'next/router'
 import { getRequestStatus } from '../../../lib/status'
 
-export default async (req, res) => {
+export default (req, res) => {
     const {
         query: { request },
     } = req
 
+    // let status = await getRequestStatus(
+    //     process.env.s3.bucketName, req.query.request);
+    // writeReponse(req.query.request, status);
+
+    getRequestStatus(process.env.s3.bucketName, req.query.request)
+        .then((status) => {
+            writeReponse(req.query.request, status);
+        })
+       .catch(err => {
+            console.log("The reason for the error:", );
+        });
+}
+
+function writeReponse(requestId, status) {
     res.statusCode = 200
     res.setHeader('Content-Type', 'application/json')
     res.end(JSON.stringify({
-        request: req.query.request,
-        status: await getRequestStatus(
-            process.env.s3.bucketName, req.query.request)
+        request: requestId,
+        status: status
     }))
 }
