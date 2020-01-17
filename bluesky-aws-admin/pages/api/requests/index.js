6d63e0f7b22c20f32459532b6a9bcfa93bbb022d
Joel Dubowy <jdubowy@gmail.com>
2020-01-17 12:53:10 -0800

    Add comment

--- bluesky-aws-admin/pages/api/requests/index.js
+++ bluesky-aws-admin/pages/api/requests/index.js
@@ -1,3 +1,6 @@
+// Note: as of nextjs 9.2.0, trailing slashs on urls for index APIs
+//   result in 500's.  This may get fixed in 9.2.1
+
 import { useRouter } from 'next/router'
 import { getRequests } from '../../../lib/status'
 
