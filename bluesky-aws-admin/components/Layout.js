f9fcac3086f2d6495171ebd6ba320aa7e4f0af87
Joel Dubowy <jdubowy@gmail.com>
2020-01-21 12:28:59 -0800

    Get index requests list page working

--- bluesky-aws-admin/components/Layout.js
+++ bluesky-aws-admin/components/Layout.js
@@ -11,9 +11,9 @@ export default ({ children, title = 'BlueSky AWS Admin' }) => (
             <title>{title}</title>
             <meta charSet="utf-8" />
             <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-            <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous" />
+            <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossOrigin="anonymous" />
         </Head>
-        <header class="container-fluid">
+        <header className="container-fluid">
             <Navbar bg="light" expand="lg">
               <Navbar.Brand href="#">BlueSky AWS Admin</Navbar.Brand>
               <Navbar.Toggle aria-controls="basic-navbar-nav" />
@@ -24,13 +24,12 @@ export default ({ children, title = 'BlueSky AWS Admin' }) => (
               </Navbar.Collapse>
             </Navbar>
         </header>
-
         <Container fluid={true}>
             {children}
             <footer>{'footer...'}</footer>
         </Container>
-        <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
-        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+        <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossOrigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossOrigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossOrigin="anonymous"></script>
     </div>
 )
--- bluesky-aws-admin/pages/index.js
+++ bluesky-aws-admin/pages/index.js
@@ -1,7 +1,61 @@
-import Layout from '../components/Layout'
 
-export default () => (
-  <Layout>
-    <div>...</div>
-  </Layout>
-)
+
+import Link from 'next/link';
+
+import Layout from '../components/Layout'
+import { getRequests } from '../lib/status'
+
+import fetchapi from '../lib/apifetcher'
+
+
+
+//const Index = props => {
+//    let requests = props.requests;
+//    let error = props.error
+function Index() {
+    let {data, fetchError} = fetchapi('/api/requests');
+
+    let requests = data && data.requests;
+    let error = fetchError || (data && data.error);
+    console.log(data)
+
+    return (
+        <Layout>
+            <div>
+                {error &&
+                    <Alert variant="danger">
+                        error
+                    </Alert>
+                }
+                <h4>Requests ({requests && requests.length})</h4>
+                <ul>
+                    {requests && requests.map((request, idx) => (
+                        <li key={idx}>
+                            <Link href="/requests/[id]" as={`/requests/${request.requestId}`}>
+                                <a>{request.requestId} </a>
+                            </Link> ({request.ts})
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        </Layout>
+    )
+};
+
+
+// Index.getInitialProps = async function() {
+//     // This is rendered server side, so we'l call getRequests
+//     // directly rather than use the /api/requests/ api.
+//     try {
+//         return {
+//             requests: await getRequests(process.env.s3.bucketName)
+//         }
+//     } catch {
+//         console.log("Failed to get list of requests:" + err);
+//         return {
+//             error: err
+//         };
+//     };
+// };
+
+export default Index;
