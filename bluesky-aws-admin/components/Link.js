c9b53f39eda84497b6c0424f73a377d26f2aa50a
Joel Dubowy <jdubowy@gmail.com>
2020-01-28 12:44:15 -0800

    Remove unused import

--- bluesky-aws-admin/components/Link.js
+++ bluesky-aws-admin/components/Link.js
@@ -1,7 +1,7 @@
 // Copied and modified from:
 // https://levelup.gitconnected.com/deploy-your-nextjs-application-on-a-different-base-path-i-e-not-root-1c4d210cce8a
 
-import NextLink, { LinkProps } from 'next/link'
+import NextLink from 'next/link'
 import { format } from 'url'
 import getConfig from 'next/config'
 
