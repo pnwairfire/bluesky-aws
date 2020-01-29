9c7e5f836f945104bc23d80cbe6be5e1293a0298
Joel Dubowy <jdubowy@gmail.com>
2020-01-28 16:57:34 -0800

    WIP on RequestStatus component

--- bluesky-aws-admin/components/RequestStatus.js
+++ bluesky-aws-admin/components/RequestStatus.js
@@ -1,26 +1,40 @@
 import Alert from 'react-bootstrap/Alert'
+import Spinner from 'react-bootstrap/Spinner'
 
 import styles from './RequestStatus.module.css'
 
 
 export default function RequestStatus(props) {
+    let systemState = props.status && props.status.system_state;
+    let systemError = !! props.status && props.status.system_error;
+    let systemMessage = props.status && props.status.system_message || '(none)';
+
     return (
         <div className={styles['request-status']}>
-            {props && props.status && (
+            {props.error &&
+                <Alert variant="danger">
+                    {props.error}
+                </Alert>
+            }
+            {props.loading && (
+                <div className={styles['loading-spinner']}>
+                    <Spinner animation="border" role="status" size="sm">
+                    </Spinner>
+                    <span>Loading...</span>
+                </div>
+            ) || (
                 <div>
                     <div className={styles['page-section']}>
                         <div><b>Request</b>: {props.request}</div>
-                        <div><b>System</b>: {props.status.system_state}</div>
-                        {props.status.system_error && (
-                            <Alert variant="danger">{props.status.system_error}</Alert>
+                        <div><b>System</b>: {systemState}</div>
+                        {systemError && (
+                            <Alert variant="danger">{systemError}</Alert>
                         )}
-                        <div><b>Message</b>: {props.status.system_message || '(none)'}</div>
+                        <div>
+                            <b>Message</b>: {systemMessage}
+                        </div>
                     </div>
                 </div>
-            ) || (
-                <Alert variant="warning">
-                    (no request status information)
-                </Alert>
             )}
         </div>
     )
