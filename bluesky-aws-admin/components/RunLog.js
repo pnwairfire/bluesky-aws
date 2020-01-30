23de79f5e0988e4115c57a0b089f80a8fcb1094b
Joel Dubowy <jdubowy@gmail.com>
2020-01-29 20:54:13 -0800

    Don't make run log api request until request and run ids are defined

--- bluesky-aws-admin/components/RunLog.js
+++ bluesky-aws-admin/components/RunLog.js
@@ -5,24 +5,29 @@ import { ApiClient } from '../lib/apiutils'
 import styles from './RunLog.module.css'
 
 export default function RunLog(props) {
-    let path ='/api/requests/' + props.request + '/runs/' + props.run + '/log'
-    let {data, fetchError} = ApiClient.get(path);
+    if (props && props.request && props.run) {
+        let path ='/api/requests/' + props.request + '/runs/' + props.run + '/log'
+        let {data, fetchError} = ApiClient.get(path);
 
-    let log = data && data.log;
-    let error = fetchError || (data && data.error);
+        let log = data && data.log;
+        let error = fetchError || (data && data.error);
 
-    return (
-        <div>
-            <h5>Run Log</h5>
-            {!data &&
-                <LoadingSpinner />
-            }
-            {error &&
-                <Alert variant="danger">{error}</Alert>
-            }
-            {log &&
-                <textarea className={styles.logtext} value={log} disabled />
-            }
-        </div>
-    )
+        return (
+            <div>
+                <h5>Run Log</h5>
+                {!data &&
+                    <LoadingSpinner />
+                }
+                {error &&
+                    <Alert variant="danger">{error}</Alert>
+                }
+                {log &&
+                    <textarea className={styles.logtext} value={log} disabled />
+                }
+            </div>
+        )
+    } else {
+        // need to explicitly return null so that nothing is rendered
+        return null
+    }
 };
