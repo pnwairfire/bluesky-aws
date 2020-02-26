df388d9612fbf249f50ca56200b751b3e351a743
Joel Dubowy <jdubowy@gmail.com>
2020-02-26 11:56:47 -0800

    Add dev script to run py.test in docker with local dirs mounted

---
+++ dev/scripts/run-pytest.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+docker run --rm -ti --user blueskyaws \
+    -v $PWD/:/bluesky-aws/ \
+    -v $PWD/.aws/:/home/blueskyaws/.aws/ \
+    -v $HOME/.ssh:/home/blueskyaws/.ssh \
+    bluesky-aws \
+    py.test
+
+#    -v $HOME/code/pnwairfire-afaws/afaws/:/bluesky-aws/afaws/ \
