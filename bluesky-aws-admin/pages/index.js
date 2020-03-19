bc050a9f0ce8caeb68b042fc9d89bd4b5e33ce3a
Joel Dubowy <jdubowy@gmail.com>
2020-03-19 12:43:07 -0700

    Implement pagination by month - rough initial version

--- bluesky-aws-admin/pages/index.js
+++ bluesky-aws-admin/pages/index.js
@@ -1,6 +1,12 @@
+import { useRouter } from 'next/router'
 import { Component } from 'react';
 import Breadcrumb from 'react-bootstrap/Breadcrumb'
 import Button from 'react-bootstrap/Button'
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import Calendar from 'react-calendar';
+import strftime from 'strftime'
 
 import Layout from '../components/Layout'
 import RequestsTable from '../components/RequestsTable'
@@ -8,15 +14,31 @@ import { ApiClient, fetcher } from '../lib/apiutils'
 import styles from './index.module.css'
 
 
-//const Index = props => {
-//    let requests = props.requests;
-//    let error = props.error
-export default class Index extends Component {
+// TODO: To support deep linking to a specific month, we could use
+//    A query param, such as '?month=202003'.  We could use 'useRouter'
+//    access the query string, but that would requred wrapping the
+//    class component 'Page' in function component - something like
+//    following
+// export default function Index() {
+//     let today = new Date();
+//     const router = useRouter()
+//     const { month } = router.query
+//     let monthObj = moment(month, "YYYYMM")
+
+//     // console.log(month)
+
+//     return (
+//         <Page month={monthObj} />
+//     )
+// }
+
+export default class Page extends Component {
 
     constructor(props) {
         super(props);
 
         this.state = {
+            month: props.month || new Date(),
             requests: [],
             error: null,
             // continuation tokens for previous set, this set, and next
@@ -28,10 +50,15 @@ export default class Index extends Component {
         // this.handleNextClick = this.handleNextClick.bind(this)
     }
 
+
     loadRequests(nextTokensIdx) {
         nextTokensIdx = nextTokensIdx || 0;
-        let query = (nextTokensIdx && this.state.nextTokens[nextTokensIdx])
-            ? ({next: this.state.nextTokens[nextTokensIdx]}) : ({})
+        let query = {
+            datePrefix: strftime('%Y%m', this.state.month)
+        }
+        if (nextTokensIdx && this.state.nextTokens[nextTokensIdx]) {
+            query.next = this.state.nextTokens[nextTokensIdx]
+        }
 
         console.log('query: ' + JSON.stringify(query));
 
@@ -62,14 +89,23 @@ export default class Index extends Component {
                         error:"Failed to load requests",
                         loading: false
                     })
-
             })
         })
-
     }
 
     componentDidMount() {
         this.loadRequests();
+    }
+
+    handleMonthChange = date => {
+        console.log("selected date: " + date);
+        this.setState({
+            month: date,
+            nextTokens: [null],
+            nextTokensIdx: 0
+        }, () => {
+            this.loadRequests(this.state.nextTokensIdx);
+        })
     }
 
     handlePreviousClick = data => {
@@ -87,12 +123,25 @@ export default class Index extends Component {
     render() {
         let prevDisabled = this.state.nextTokensIdx <= 0;
         let nextDisabled = ! this.state.nextTokens[this.state.nextTokensIdx + 1]
-        console.log(prevDisabled + '/' +nextDisabled);
+        //console.log(prevDisabled + '/' +nextDisabled);
         return (
             <Layout>
                 <Breadcrumb>
                     <Breadcrumb.Item active>Home</Breadcrumb.Item>
                 </Breadcrumb>
+                <Container fluid={true}>
+                    <Row>
+                        <Col sm="2">
+                            <Calendar
+                                defaultView="year"
+                                onChange={this.handleMonthChange}
+                                value={this.state.month}
+                                maxDetail="year"
+                                minDetail="year"
+                            />
+                        </Col>
+                    </Row>
+                </Container>
                 <RequestsTable
                     loading={this.state.loading}
                     requests={this.state.requests}
